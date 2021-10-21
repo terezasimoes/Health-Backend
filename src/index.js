00bd5a45bc98cec1575787f48c9ae1bab5e6a716
Tereza Crisitina <terezasimoes@gmail.com>
2021-10-21 09:03:32 -0300

    creating server and adding routes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = require('./Routers');
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/register', router.register);
+
+app.listen(3000, console.log(`Server listening on port ${3000}`));
