bc4abc737e4a09d8cbc642e3525098c151aac085
Tereza Crisitina <terezasimoes@gmail.com>
2021-10-22 07:20:37 -0300

    jsonwebtoken

--- src/Routers/register.js
+++ src/Routers/register.js
@@ -1,12 +1,22 @@
 const {Router} = require('express');
+const jwt = require('jsonwebtoken');
 
 const register = Router();
 
 register.post('/',(req, res) => {
   try {
     const data = req.body;
-    return res.status(201).json("Requisição bem sucedida");
+    const token = jwt.sign({
+      "userID": 1, "name": `${data.firstName} ${data.lastName}`},
+      "nasa", {expiresIn: "5d"});
+    
+    data.token = token;
+
+
+    return res.status(201).json(data);
   }catch {
     return res.status(400).json("Requisição mal sucedida");
   } 
 });
+
+module.exports = {register};
